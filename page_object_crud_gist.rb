4674779168bfe7fd3a763325312c5def88674686
RamadhanaRey <reynaldiramadhana1995@gmail.com>
2019-10-24 13:42:38 +0700

    Update page_object_crud_gist.rb

--- page_object_crud_gist.rb
+++ page_object_crud_gist.rb
@@ -1,9 +1,9 @@
 class CrudGistPage < SitePrism::Page
 
- element :username, 		    :xpath, 	"//input[@id='login_field']"
- element :password, 		    :xpath, 	"//input[@id='password']"
- element :button_sign_in, :xpath, 	"//input[@type='submit']"
- element :create_gist, 		 :xpath, 	"//*[text()='Create a gist']"	
+ element :username, 		    :xpath, 	   "//input[@id='login_field']"
+ element :password, 		    :xpath, 	   "//input[@id='password']"
+ element :button_sign_in, :xpath, 	   "//input[@type='submit']"
+ element :create_gist, 		 :xpath, 	   "//*[text()='Create a gist']"	
  element :description, 		 :xpath,     "(//input[@class='form-control input-block input-contrast'])"
  element :filename, 		    :xpath,     "(//input[@class='form-control filename js-gist-filename js-blob-filename'])"
  element :content, 			    :xpath,     "//div[@class='CodeMirror-gutter CodeMirror-linenumbers']"
